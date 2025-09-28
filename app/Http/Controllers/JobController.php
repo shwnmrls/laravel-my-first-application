3504a7d616a526edbfc5f1310eab6058908a0eef
shwnmrls <shawnmorales39@gmail.com>
2025-09-28 21:03:30 +0800

    Chapter 7: Refactor job routes to a resourceful controller

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use App\Models\Employer;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('jobs.index', [
+            'jobs' => Job::with(['employer', 'tags'])->paginate(3)
+        ]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create', [
+            'employers' => Employer::all(),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]);
+
+        Job::create([
+            'title' => $request->input('title'),
+            'salary' => $request->input('salary'),
+            'employer_id' => $request->input('employer_id', 1), // fallback to 1
+        ]);
+
+        return redirect('/jobs');
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    public function edit(Job $job)
+    {
+        $employers = Employer::all();
+        return view('jobs.edit', compact('job', 'employers'));
+    }
+
+    public function update(Request $request, Job $job)
+    {
+        $request->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]);
+
+        $job->update([
+            'title' => $request->input('title'),
+            'salary' => $request->input('salary'),
+        ]);
+
+        return redirect('/jobs/' . $job->id);
+    }
+
+    public function destroy(Job $job)
+    {
+        $job->delete();
+        return redirect('/jobs');
+    }
 }
